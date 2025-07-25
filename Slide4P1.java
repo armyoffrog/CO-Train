df2c27527e96acba65e2c4308e603c0831a34c96
armyoffrog <sidneyfroge@gmail.com>
2025-07-24 20:13:23 -0700

    Even Odd

---
+++ Slide4P1.java
@@ -0,0 +1,24 @@
+
+public class Slide4P1 {
+
+    static boolean evenOdd(int number){
+
+        if (number % 2 == 0){
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
+
+} 
